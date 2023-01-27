1cc6251b92047799c8f8b6d69242055e70ccd817
pruiz <pruiz@DGIIDATN13>
2023-01-27 12:15:15 -0400

    Add project files.

---
+++ ClientGRPC/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using ClientGRPC;
+
+using (var channel = GrpcChannel.ForAddress("https://localhost:7028"))
+{
+    var client = new Greeter.GreeterClient(channel);
+    var reply = await client.SayHelloAsync(new HelloRequest
+    {
+        Name = "Pedro"
+    });
+
+    Console.WriteLine(reply.Message);
+
+    var response = await client.GetDataAsync(new DataRequest { });
+
+    foreach (var languaje in response.Languages)
+    {
+        Console.WriteLine(languaje);
+    }
+
+    Console.ReadKey();
+
+
+}
